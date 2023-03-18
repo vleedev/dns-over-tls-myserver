dca6a40a0e7f5da39fdbcffd7d4f425d508dae5a
LEE TUAN <me@vlee.dev>
2023-03-18 20:41:40 +0700

    Use some variables in google provider

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "google" {
   # The google name must be defined first in terraform required_providers
   # We are using terraform cloud
   # Google credentials is set in terraform workspace variable ENV GOOGLE_CREDENTIALS
-  project = "default-259811"
-  region  = "us-west1"
-  zone    = "us-west1-c"
+  project = var.google_project
+  region  = var.google_region
+  zone    = var.google_zone
 }
