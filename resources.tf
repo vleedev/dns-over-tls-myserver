2084499153315093a14cefe0f77a3a236722e752
LEE TUAN <me@vlee.dev>
2023-03-19 03:55:21 +0700

    Add project variable type object

--- resources.tf
+++ resources.tf
@@ -1,7 +1,7 @@
-resource "google_compute_network" "dns-over-tls-network" {
+resource "google_compute_network" "project-network" {
   # Struct: type - resource local name
   # Type: required, check https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
   # Resource local name: it must be unique in the terraform tool
   # Terraform will look up the type in terraform module
-  name = "dns-over-tls-network"
+  name = "${var.project.name}-network"
 }
--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "google_project" {}
 variable "google_region" {}
 # This empty variable will be overrided by terraform cloud variable
 variable "google_zone" {}
+# Variable with object type
+variable "project" {
+  type = object({
+    name = string
+  })
+  default = {
+    name = "dns-over-tls"
+  }
+}
