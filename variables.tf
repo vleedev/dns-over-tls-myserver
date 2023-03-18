43f347e45dc2bed0c402d1697aadb938f4cad78e
LEE TUAN <me@vlee.dev>
2023-03-18 20:54:48 +0700

    Use some variables in google provider

---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "google_project" {} # This empty variable will be overrided by terraform cloud variable
+variable "google_region" {} # This empty variable will be overrided by terraform cloud variable
+variable "google_zone" {} # This empty variable will be overrided by terraform cloud variable
